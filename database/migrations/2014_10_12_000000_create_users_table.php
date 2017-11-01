e5f2214d1b7a6590a6dd895c770c288532ca2eaf
Unknown <jchavzr@gmail.com>
2017-11-01 09:03:40 -0600

    Modifiacion de campos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
         //    $table->string('uid');
             $table->string('id_compania');
-            $table->string('archivo');
+            $table->string('archivo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
